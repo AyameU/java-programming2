e436172fcd23ecced0db9bcc2b5e2c591c982213
AyameU <54376512+AyameU@users.noreply.github.com>
2020-12-19 17:49:14 -0600

    Solution for 08_02 Average of positive numbers

--- part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -6,5 +6,22 @@ public class AverageOfPositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int input = 0;
+        int sum = 0;
+        int count = 0;
+
+        do {
+            input = Integer.parseInt(scanner.nextLine());
+            if(input > 0) {
+                sum += input;
+                count++;
+            }
+        } while(input != 0);
+
+        if(count == 0) {
+            System.out.println("Cannot calculate the average");
+        } else {
+            System.out.println(sum / (double) count);
+        }
     }
 }
