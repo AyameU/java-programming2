f94f0b3ce1a50673792e44639c07ada8f7ed25ca
AyameU <54376512+AyameU@users.noreply.github.com>
2020-12-19 17:47:37 -0600

    Solution for 08_01 Cubed

--- part08-Part08_01.Cubes/src/main/java/Cubes.java
+++ part08-Part08_01.Cubes/src/main/java/Cubes.java
@@ -5,6 +5,16 @@ public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String input = "";
 
+        while(!input.equals("end")) {
+            input = scanner.nextLine();
+
+            if(!input.equals("end")) {
+                int num = Integer.parseInt(input);
+                int cubed = num * num * num;
+                System.out.println(cubed);
+            }
+        }
     }
 }
