b789fdfd107d2375cd70846ff2f27509ae956eea
AyameU <54376512+AyameU@users.noreply.github.com>
2020-12-20 15:36:06 -0600

    Solution for 08_03, if blocks are messy, should refactor

--- part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
+++ part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
@@ -6,8 +6,12 @@ public class LiquidContainers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        int first = 0;
+        int second = 0;
 
         while (true) {
+            System.out.println("First: " + first + "/100");
+            System.out.println("Second: " + second + "/100");
             System.out.print("> ");
 
             String input = scan.nextLine();
@@ -15,6 +19,53 @@ public class LiquidContainers {
                 break;
             }
 
+            String[] parts = input.split(" ");
+            String command = parts[0];
+            int amount = Integer.valueOf(parts[1]);
+
+            switch(command) {
+                case "add":
+                    if(first + amount <= 100 && amount >= 0) {
+                        first += amount;
+                    }
+                    else {
+                        first = 100;
+                    }
+                    break;
+                case "move":
+                    //if amount is greater than amount in first container
+                    // move all of first container liquid to second
+                    if(amount >= 0) {
+                        if(amount > first) {
+                            if(second + first <= 100) {
+                                second += first;
+                            }
+                            else {
+                                second = 100;
+                            }
+                            first = 0;
+                        }
+                        else if(second + amount <= 100 && first - amount >= 0) {
+                            first -= amount;
+                            second += amount;
+                        }
+                        else {
+                            first = 0;
+                            second = 100;
+                        }
+                    }
+                    break;
+                case "remove":
+                    if(amount >= 0) {
+                        if(amount > second) {
+                            second = 0;
+                        }
+                        else {
+                            second -= amount;
+                        }
+                    }
+                    break;
+            }
         }
     }
 
