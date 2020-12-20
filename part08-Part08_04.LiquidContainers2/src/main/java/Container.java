00f5f7f1f39934a3ba230494a8c8d3702bd21d7f
AyameU <54376512+AyameU@users.noreply.github.com>
2020-12-20 16:37:59 -0600

    Solution for 08_04, refactored 08_03 by separating the container logic from the client application

--- part08-Part08_04.LiquidContainers2/src/main/java/Container.java
+++ part08-Part08_04.LiquidContainers2/src/main/java/Container.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
-
 public class Container {
+
+    private int volume = 0;
+    private int maxVolume = 100;
+
+    public Container() {}
+
+    public int contains() {
+        return volume;
+    }
+
+    public void add(int amount) {
+        if(amount >= 0) {
+            if(volume + amount <= maxVolume) {
+                volume += amount;
+            }
+            else {
+                volume = 100;
+            }
+        }
+    }
+
+    public void remove(int amount) {
+        if(amount >= 0) {
+            if(volume - amount >= 0) {
+                volume -= amount;
+            }
+            else {
+                volume = 0;
+            }
+        }
+    }
+
+    public String toString() {
+        return volume + "/" + maxVolume;
+    }
 }
--- part08-Part08_04.LiquidContainers2/src/main/java/LiquidContainers2.java
+++ part08-Part08_04.LiquidContainers2/src/main/java/LiquidContainers2.java
@@ -6,8 +6,12 @@ public class LiquidContainers2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        Container firstContainer = new Container();
+        Container secondContainer = new Container();
 
         while (true) {
+            System.out.println("First: " + firstContainer.contains() + "/100");
+            System.out.println("Second: " + secondContainer.contains() + "/100");
             System.out.print("> ");
 
             String input = scan.nextLine();
@@ -15,7 +19,28 @@ public class LiquidContainers2 {
                 break;
             }
 
+            String[] parts = input.split(" ");
+            String command = parts[0];
+            int amount = Integer.valueOf(parts[1]);
+
+            switch (command) {
+                case "add":
+                    firstContainer.add(amount);
+                    break;
+                case "move":
+                    if(firstContainer.contains() >= amount) {
+                        firstContainer.remove(amount);
+                        secondContainer.add(amount);
+                    }
+                    else if(firstContainer.contains() < amount) {
+                        secondContainer.add(firstContainer.contains());
+                        firstContainer.remove(firstContainer.contains());
+                    }
+                    break;
+                case "remove":
+                    secondContainer.remove(amount);
+                    break;
+            }
         }
     }
-
 }
