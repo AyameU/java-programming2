f485ce4ca1de0194f83657ef1a647fb52c1ed718
AyameU <54376512+AyameU@users.noreply.github.com>
2020-12-20 19:08:24 -0600

    08_05 Solution

--- part08-Part08_05.TodoList/src/main/java/TodoList.java
+++ part08-Part08_05.TodoList/src/main/java/TodoList.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
-public class ToDoList {
+public class TodoList {
+
+    private ArrayList<String> tasks = new ArrayList<>();
+
+    public TodoList() {}
+
+    public void add(String task) {
+        tasks.add(task);
+    }
+
+    public void print() {
+        for(int i = 0, j = 1; i < tasks.size(); i++, j++) {
+            System.out.println(j + ": " + tasks.get(i));
+        }
+    }
+
+    public void remove(int number) {
+        tasks.remove(number - 1);
+    }
 }
--- part08-Part08_05.TodoList/src/main/java/UserInterface.java
+++ part08-Part08_05.TodoList/src/main/java/UserInterface.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class UserInterface {
+
+    private TodoList todoList;
+    private Scanner scanner;
+
+    public UserInterface(TodoList todoList, Scanner scanner) {
+        this.todoList = todoList;
+        this.scanner = scanner;
+    }
+
+    public void start() {
+
+        while(true) {
+            System.out.print("Command: ");
+            String command = scanner.nextLine();
+
+            if(command.equals("stop")) {
+                break;
+            }
+            else {
+                switch(command) {
+                    case "add":
+                        System.out.print("To add: ");
+                        String task = scanner.nextLine();
+                        todoList.add(task);
+                        break;
+                    case "list":
+                        todoList.print();
+                        break;
+                    case "remove":
+                        System.out.print("Which one is removed? ");
+                        int number = Integer.parseInt(scanner.nextLine());
+                        todoList.remove(number);
+                        break;
+                }
+            }
+        }
+    }
 }
